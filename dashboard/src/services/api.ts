7ed15ab33915b4cb5fe9decb3b9de9f23d09d37a
Anas Youssef <anasyoussef649@gmail.com>
2026-01-11 22:09:48 +0200

    update book journey

--- dashboard/src/services/api.ts
+++ dashboard/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: "https://api.hadith-shareef.com/api",
+  baseURL: "http://localhost:4000/api",
 });
 
 // Request interceptor
